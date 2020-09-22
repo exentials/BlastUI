2a10935b89d4a671fe3cb79cdada1928ce5d7ab0
Damiano <damiano.g@outlook.it>
2020-09-22 23:55:51 +0200

    FIX changes

--- BlastUI.Components/wwwroot/blastui.js
+++ BlastUI.Components/wwwroot/blastui.js
@@ -3,20 +3,13 @@
 
 import { createColorPalette } from "./microsoft/fast-components/dist/fast-components.min.js";
 
-export class BlastUI {
-
-    createPalette(baseColor) {
-        return createColorPalette(baseColor);
-    }
-}
 
 window.blastUI = {
     getPalette: (color) => {
         if (color) {
-            var bui = new BlastUI();
-            var palette = bui.createPalette(color);
-            // var palette = createColorPalette(color);
-            return palette;
+
+            var palette = createColorPalette(color);
+            return [];
         }
     }
 }
